1b531e295899f7a79c2a9ec28321e0c04d3130e3
joel kirabo <kirabojoel001@gmail.com>
2024-11-05 20:42:54 +0300

    sooka ondekekek

--- actions/contacts.ts
+++ actions/contacts.ts
@@ -2,17 +2,15 @@
 import { ContactProps } from "@/types/types";
 import {db } from "@/lib/db";
 
-
 export default async function getContacts(){
-    const contacts = await db.contacts.findMany() || [];
-return contacts;}
+    const contacts = await db.contacts.findMany();
+    return contacts;}
 
 export async function saveContacts(data:ContactProps){
   try {
-    const newcontact = await db.contacts.create({
+     await db.contacts.create({
       data
     })
-    console.log( ` ${newcontact} has been added`)
   } catch (error) {
     console.log(error)
   }
