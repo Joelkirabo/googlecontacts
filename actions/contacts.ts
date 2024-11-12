bc5f583858c3ce8052b32c62632221b25e2e648f
joel kirabo <kirabojoel001@gmail.com>
2024-11-12 20:41:10 +0300

    jhbfhjbfhjsdbj

--- actions/contacts.ts
+++ actions/contacts.ts
@@ -3,7 +3,7 @@ import { ContactProps } from "@/types/types";
 import {prisma } from "@/lib/prisma";
 
 
-
+//get contacts from db using prisma
 export default async function getContacts(){
     const contacts = await prisma.contacts.findMany();    
     return contacts;
@@ -14,6 +14,7 @@ export async function saveContacts(data:ContactProps){
      await prisma.contacts.create({
       data
     })
+  
 
   } catch (error) {
     console.log(error)
